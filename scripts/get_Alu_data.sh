63a14f0ab59bd8636c30d16fcf22a22dbce17fb1
mglubber <mark.g.lubberts@gmail.com>
2016-07-13 15:20:37 -0400

    remove duplicate alu position script, updated comments on new one

--- scripts/get_Alu_data.sh
+++
@@ -1,1 +0,0 @@
-grep "Alu" hg38.fa.out | sed -r s/s+/t/g | cut --complement -f2,12 | column -t > ALU_data_extended.txt
--- scripts/get_Alu_position_new.sh
+++ scripts/get_Alu_position_new.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # get Alu positions from UCSC hg38 repeatMasker file. Include overlap of the ALU
 # element with the database consensus sequence
+
+# usage: "./get_Alu_positions_new.sh hg38.fa.out"
 FILE="$1"
-echo "CHROM	START	END	SOMETHING	STRAND	FAMILY	E_MISS	E_START	E_END"
-grep 'Alu' "$FILE" | sed -r -e 's/\s+/\t/g' -e 's/\tC\t/\t-\t/'| cut -f 6-11,13-15
+# Add headers to the top of the file for use in R. Respectively
+# Chromosome(query) name, query start, query end, strand (+ or -), Repeat Family, Consensus start, end, missing
+# Consensus positions start relative to consensus sequence (pos 1), end is final position in consensus, missing
+# is the number of bp from end of match to the end of the consensus sequence (i.e. if the repeat match starts 
+# from the 5 bp of a 215bp consensus sequence and ends at the 200th, Con_Start, Con_End, and Con_Miss will be 
+# 5, 200, (15) respectively. If the repeate is in the reverse strand, the order of  Con_Start, Con_End, and 
+# Con_Miss are reversed (i.e. (15), 200, 5)
+echo "CHROM,START,END,STRAND,FAMILY,Con_Start,Con_End,Con_Miss" 
+# grep the file for Alu repeats, replace all whitespace with tabs, change the negative strange from C to -,
+# and remove unneccesary columns. 
+grep 'Alu' "$FILE" | sed -r -e 's/^\s+//' -e 's/\s+/,/g' -e 's/,C,/,-,/'| cut -d "," -f 5-7,9-10,12-14
