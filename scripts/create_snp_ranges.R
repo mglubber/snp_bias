26ee73a5fd9ed45203a8d9018a43610b5fc24233
mglubber <mark.g.lubberts@gmail.com>
2016-07-18 16:26:58 -0400

    Updated get_Alu_position_new.sh to allow for arbitrary filtering of the hg38.fa.out file, then renamed that script and the old get_alu_position.sh script to reflect changes. Updated create_snp_ranges to add new info to the range data, and process .csv file for repeat positions. Updated protocol file with changes to scripts/sequence.

--- scripts/create_snp_ranges.R
+++ scripts/create_snp_ranges.R
@@ -1,7 +1,23 @@
 library("GenomicRanges")
-alu_table <- read.table("data/ALU_locations.txt",sep='\t',header=T)
-alu_ranges <- GRanges(seqname=alu_table$CHROM,ranges=IRanges(start=alu_table$START,end=alu_table$END),strand=alu_table$STRAND,alufamily=alu_table$FAMILY)
+alu_file <- "data_int/ALU_locations.txt"
+snp_file <- "data_int/allsnpsmodified.txt"
+
+alu_table <- read.csv(alu_file,header=T)
+cat("Created table of alu elements from file", alu_file, "\n")
+cat(nrow(alu_table), "repeat elements found.\n")
+# flip Con_Start and Con_End for negative strand repeats, so that column names are correct
+alu_table[alu_table$STRAND=="-",c(6,8)] <- alu_table[alu_table$STRAND=="-",c(8,6)]
+cat("Flipped consensus sequence start positions and truncated base pairs for negative strand\n")
+# Convert the table to a set of genomic ranges, with additional information
+alu_ranges <- GRanges(seqname=alu_table$CHROM,ranges=IRanges(start=alu_table$START,end=alu_table$END),strand=alu_table$STRAND,alufamily=alu_table$FAMILY,conStart=alu_table$Con_Start,conEnd=alu_table$Con_End,conMiss=alu_table$Con_Miss)
+cat("Created", length(alu_ranges), "repeat element genomic ranges\n")
+# Split alu_ranges in positive and negative ranges, to simplify analysis of downstream/upstream SNPs
 alu_positive <- alu_ranges[strand(alu_ranges)=="+"]
 alu_negative <- alu_ranges[strand(alu_ranges)=="-"]
-snp_table <- read.table("genomicsproject/allsnpsmodified", sep='\t', header=T)
-snp_ranges <- GRanges(seqname=snp_table$chrom,ranges=IRanges(start=snp_table$chromStart,end=snp_table$chromEnd),strand=snp_table$strand,name=snp_table$name)
+cat("Split positive and negative strand genomic ranges to simplify finding overlaps.\n Loading SNP/mutation data, this make take some time.\n")
+# Read snp/other mutations locations from file
+snp_table <- read.table(snp_file, sep='\t', header=T)
+cat("Created table of", nrow(snp_table), "SNP/Mutation positions and types from", snp_file, ".\n")
+# Create genomic ranges from snp/mutation tables
+snp_ranges <- GRanges(seqname=snp_table$chrom,ranges=IRanges(start=snp_table$chromStart,end=snp_table$chromEnd),strand=snp_table$strand,name=snp_table$name,observed=snp_table$observed,moltype=snp_table$molType,class=snp_table$class)
+cat("Created", length(snp_ranges), "SNP/Mutation genomic ranges.\n")
--- scripts/get_repeat_positions.sh
+++ scripts/get_repeat_positions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# get Alu positions from UCSC hg38 repeatMasker file. Include overlap of the ALU
+# get repeate positions from UCSC hg38 repeatMasker file. Include overlap of the repeat
 # element with the database consensus sequence
 
-# usage: "./get_Alu_positions_new.sh hg38.fa.out"
+# usage: "./get_repeat_positions.sh hg38.fa.out"
 FILE="$1"
+REPEAT_FILTER="$2"
 # Add headers to the top of the file for use in R. Respectively
 # Chromosome(query) name, query start, query end, strand (+ or -), Repeat Family, Consensus start, end, missing
 # Consensus positions start relative to consensus sequence (pos 1), end is final position in consensus, missing
@@ -12,6 +13,6 @@ FILE="$1"
 # 5, 200, (15) respectively. If the repeate is in the reverse strand, the order of  Con_Start, Con_End, and 
 # Con_Miss are reversed (i.e. (15), 200, 5)
 echo "CHROM,START,END,STRAND,FAMILY,Con_Start,Con_End,Con_Miss" 
-# grep the file for Alu repeats, replace all whitespace with tabs, change the negative strange from C to -,
-# and remove unneccesary columns. 
-grep 'Alu' "$FILE" | sed -r -e 's/^\s+//' -e 's/\s+/,/g' -e 's/,C,/,-,/'| cut -d "," -f 5-7,9-10,12-14
+# grep the file for repeats matching the filter, replace all whitespace with tabs, change the negative strand
+# from C to -, remove bracket from missing bp, and remove unneccesary columns. 
+grep "$REPEAT_FILTER" "$FILE" | sed -r -e 's/^\s+//' -e 's/\s+/,/g' -e 's/,C,/,-,/' -e 's/\(([0-9]+)\)/\1/g' | cut -d "," -f 5-7,9-10,12-14
