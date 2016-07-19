31fdea5f24b1b6cf27e5d02af0f04715c9ecd6c7
mglubber <mark.g.lubberts@gmail.com>
2016-07-19 12:19:12 -0400

    Updated snp_distance_histogrm.R to produce images of the consensus senquence start position and number of missing base pairs. Also switched to Cairo for image creation instead of jpeg() because of weird X11 errors. Updated protocol with new information regarding the number of Alu elements that are split up.

--- scripts/snp_distance_histogram.R
+++ scripts/snp_distance_histogram.R
@@ -1,4 +1,7 @@
+# Create a histogram of the # of bp truncated from the end of the repeat element
+library(Cairo)
 require(lattice)
-jpeg(filename="alu_element_histogram.jpg")
-histogram(~distance|alufamily,data=distance_vs_family,type="density",breaks=50)
+filename="alu_element_histogram.jpg"
+Cairo(file=filename,type="jpeg",units="in",width=14,height=11,dpi=72)
+histogram(~conMiss|alufamily,data=alu_ranges,type="density",breaks=50)
 dev.off()
